d8d3960d90d45ca2434d012673ab1b605d9a201b
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-02-25 10:16:02 -0800

    hashmaps

--- src/pages/lessons/index.tsx
+++ src/pages/lessons/index.tsx
@@ -2,10 +2,21 @@ import Link from "next/link";
 
 export default function Lessons() {
 	return (
-		<ol>
-			<li>
-				<Link href="/lessons/dsa">Data Structures</Link>
-			</li>
-		</ol>
+		<div>
+			<ol>
+				<li>
+					<Link href="/lessons/dsa">Data Structures</Link>
+				</li>
+				<li>
+					<Link href="/lessons/comporg">
+						Computer Organization & Assembly
+					</Link>
+				</li>
+				<li>
+					<Link href="/lessons/discrete">Discrete Math</Link>
+				</li>
+			</ol>
+			<p>More to come!</p>
+		</div>
 	);
 }
